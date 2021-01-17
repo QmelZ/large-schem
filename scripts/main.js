e43e1c65fa1c169dafb987ed917f8dfca2d635a4
QmelZ <59574967+QmelZ@users.noreply.github.com>
2021-01-17 03:03:39 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,83 +1,5 @@
-// function \/ by ZestyLemonade, ty :)
-function select(bind, color, onSelect){
-  this.color = color.cpy();
-  this.bcolor = color.cpy().sub(0.2, 0.2, 0.2, 0);
-  this.startX = 0;
-  this.startY = 0;
-  this.clearing = false;
-  this.bind = Packages.arc.input.KeyCode[bind];
-  Events.run(Trigger.update, (e) => {
-    if(!Vars.state.isPlaying()) return;
-    if(Core.input.keyDown(this.bind)){
-      if(!this.clearing){
-        this.clearing = true;
-        this.startX = mouseX();
-        this.startY = mouseY();
-      }
-    } else {
-      if(!this.clearing) return;
-      this.clearing = false;
-      let endX = mouseX();
-      let endY = mouseY();
-      if(endX < this.startX){
-        let tmp = this.startX;
-        this.startX = endX;
-        endX = tmp;
-      }
-      if(endY < this.startY){
-        let tmp = this.startY;
-        this.startY = endY;
-        endY = tmp;
-      }
-
-      onSelect(this.startX, this.startY, endX, endY);
-    }
-  });
-
-  Events.run(Trigger.postDraw, (e) => {
-    if(this.clearing){
-      Lines.stroke(1);
-      const result = Placement.normalizeDrawArea(
-        Blocks.air,
-        this.startX,
-        this.startY,
-        mouseX(),
-        mouseY(),
-        false,
-        100,
-        1
-      );
-
-      Lines.stroke(2);
-
-      Draw.color(this.bcolor);
-      Lines.rect(
-        result.x,
-        result.y - 1,
-        result.x2 - result.x,
-        result.y2 - result.y
-      );
-      Draw.color(this.color);
-      Lines.rect(
-        result.x,
-        result.y,
-        result.x2 - result.x,
-        result.y2 - result.y
-      );
-    }
-  });
-
-  function mouseX(){
-    return Math.floor(Vars.player.mouseX / Vars.tilesize + 0.5);
-  }
-
-  function mouseY(){
-    return Math.floor(Vars.player.mouseY / Vars.tilesize + 0.5);
-  }
-}
-
-// from this part on its my code
-let create = (x, y, x2, y2) => {
+let schem = extend(Schematics, {
+    create(x, y, x2, y2){
         let result = Placement.normalizeArea(x, y, x2, y2, 0, false, 2147483647);
         x = result.x;
         y = result.y;
@@ -133,15 +55,7 @@ let create = (x, y, x2, y2) => {
         }
 
         return new Schematic(tiles, new StringMap(), width, height);
-};
-
-let s = Vars.schematics;
-new select("t", Pal.accent, (startX, startY, endX, endY) => {
-    Vars.ui.showTextInput(
-        "Copy your schematic and click any button to close",
-        "schem:",
-        0,
-        s.writeBase64(create(startX, startY, endX, endY)),
-        () => {}
-    );
+    }
 });
+
+Vars.schematics = schem;
